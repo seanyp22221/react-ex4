7af0875cf277a43e3302d8e5deb34fe75630b404
seanyp22221 <trainer3@myamberacademy.com>
2022-03-15 22:39:16 -0500

    After adding Exercise file Notes

---
+++ src/Notes.js
@@ -0,0 +1,7 @@
+/**
+ * Create a React application that has a component called Calculator
+ * the component should have 2 input fields a button and a H1 to display results
+ * The component should have a prop called OPERATOR
+ * the Operator prop should manage whether the operation performed is 
+ * Add, Multiply, Divide or subtract
+ */
--- src/components/AgeCounter.jsx
+++ src/components/AgeCounter.jsx
@@ -15,10 +15,3 @@ const AgeCounter = (props) =>{
     );
 }
 export default AgeCounter;
-/**
- * Create a React application that has a component called Calculator
- * the component should have 2 input fields a button and a H1 to display results
- * The component should have a prop called OPERATOR
- * the Operator prop should manage whether the operation performed is 
- * Add, Multiply, Divide or subtract
- */
